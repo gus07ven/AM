d97768108b17741e8854209355ed8b59491ccce7
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2022-04-04 06:56:01 -0500

    Add selection sort

--- Sorting_algos.py
+++ Sorting_algos.py
@@ -1,6 +1,8 @@
 from typing import List
 
-
+# O(n^2), stable, in-place.
+# Pros: Good for sorting list almost sorted (almost at O(1)). Used for smaller lists in Python's Timsort.
+# Cons: Slow.
 def insertion_sort(unsorted_list: List[int]) -> List[int]:
     for i, entry in enumerate(unsorted_list):
         current = i
@@ -9,10 +11,24 @@ def insertion_sort(unsorted_list: List[int]) -> List[int]:
             current -= 1
     return unsorted_list
 
+def selection_sort(unsorted_list: List[int]) -> List[int]:
+    n = len(unsorted_list)
+    for i in range(n):
+        min_index = i
+        for j in range(i, n):
+            if unsorted_list[j] < unsorted_list[min_index]:
+                min_index = j
+        unsorted_list[i], unsorted_list[min_index] = unsorted_list[min_index], unsorted_list[i]
+    return unsorted_list
+
 
 if __name__ == '__main__':
 
     # Insertion sort
     unsorted_list = [5, 3, 1, 2, 4]
     res = insertion_sort(unsorted_list)
+    print(' '.join(map(str, res)))
+
+    unsorted_list = [5, 3, 1, 2, 4]
+    res = selection_sort(unsorted_list)
     print(' '.join(map(str, res)))
