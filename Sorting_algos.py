3b49be312eab71cffdf5a2fba56fb7316d46065a
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2022-04-06 20:49:25 -0500

    Add bubble sort

--- Sorting_algos.py
+++ Sorting_algos.py
@@ -12,6 +12,7 @@ def insertion_sort(unsorted_list: List[int]) -> List[int]:
             current -= 1
     return unsorted_list
 
+
 # O(n^2), NOT-stable, in-place
 # Pros: How most people would sort something
 # Cons: Slow.
@@ -26,14 +27,35 @@ def selection_sort(unsorted_list: List[int]) -> List[int]:
     return unsorted_list
 
 
+# O(n^2), stable, in-place
+# Pros: Stable
+# Cons: easy to mess up indexes
+def bubble_sort(unsorted_list: List[int]) -> List[int]:
+    n = len(unsorted_list)
+    for i in reversed(range(n)):
+        swapped = False
+        for j in range(i):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+                swapped = True
+        if not swapped:
+            return unsorted_list
+    return unsorted_list
+
+
 if __name__ == '__main__':
 
     # Insertion sort
     unsorted_list = [5, 3, 1, 2, 4]
-    res = insertion_sort(unsorted_list)
-    print(' '.join(map(str, res)))
+    res_is = insertion_sort(unsorted_list)
+    print(' '.join(map(str, res_is)))
 
     # Selection sort
     unsorted_list = [5, 3, 1, 2, 4]
-    res = selection_sort(unsorted_list)
-    print(' '.join(map(str, res)))
+    res_ss = selection_sort(unsorted_list)
+    print(' '.join(map(str, res_ss)))
+
+    # Bubble sort
+    unsorted_list = [5, 3, 1, 2, 4]
+    res_bs = bubble_sort(unsorted_list)
+    print(' '.join(map(str, res_bs)))
