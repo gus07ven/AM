9f943fb34fdd31026923523f4a8f9e954d398920
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2022-04-11 06:56:10 -0500

    Update Group_anagrams.py

--- Group_anagrams.py
+++ Group_anagrams.py
@@ -19,10 +19,28 @@ def group_anagrams(strs: List[str]) -> List[List[str]]:
     return res
 
 
+def group_anagrams_optimized(strs: List[str]) -> List[List[str]]:
+    ans = collections.defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        ans[tuple(count)].append(s)
+    res = list(ans.values())
+
+    for row in res:
+        row.sort()
+    res.sort(key=lambda row: row[0])
+    return res
+
+
 if __name__ == '__main__':
     input = "eat tea tan ate nat bat"
     strs = input.split()
     result = group_anagrams(strs)
     print(result)
+
+    result2 = group_anagrams_optimized(strs)
+    print(result2)
 
     
