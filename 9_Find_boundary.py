d156daccaf32d3bf7a6cc124ba0d050aa06258fd
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2022-04-21 20:10:45 -0500

    Add find boundaries

---
+++ 9_Find_boundary.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def find_boundary(arr: List[bool]) -> int:
+    low = 0
+    high = len(arr)
+
+    while low <= high:
+        mid = high + low // 2
+        if arr[mid] == 'true':
+            if arr[mid - 1] == 'false':
+                return mid
+        elif arr[mid] == 'false' and mid > 0 or mid < len(arr):
+            low = mid + 1
+        else:
+            high = mid - 1
+
+
+
+
+
